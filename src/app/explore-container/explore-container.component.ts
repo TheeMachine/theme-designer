8ebc877546ea75e15f53d909ba2ebd72c1abd732
Kasım Yüksel <kasimyueksel@gmail.com>
2021-07-16 16:20:40 +0300

    Test için hazırlandı

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
 
-@Component({
-  selector: 'app-explore-container',
-  templateUrl: './explore-container.component.html',
-  styleUrls: ['./explore-container.component.scss'],
-})
+@Component({selector: 'app-explore-container', templateUrl: './explore-container.component.html', styleUrls: ['./explore-container.component.scss']})
 export class ExploreContainerComponent implements OnInit {
-  @Input() name: string;
+    @Input()name : string;
 
-  constructor() { }
+    constructor(private fb : FormBuilder) {
+        Object.keys(this.form.value).forEach(elm => {
+           this.form.get(elm).setValue(getComputedStyle(document.documentElement).getPropertyValue('--ion-color-' + elm).trim());
+        });
+        
+    }
 
-  ngOnInit() {}
+    form = this.fb.group({
+        primary: [],
+        danger: [],
+        success: [],
+        warning: [],
+        secondary: [],
+        light: []
+    });
+
+
+    ngOnInit() {}
+
+    save() {
+        Object.keys(this.form.value).forEach(elm => {
+            document.documentElement.style.setProperty('--ion-color-' + elm, this.form.get(elm).value);
+        });
+    }
 
 }
